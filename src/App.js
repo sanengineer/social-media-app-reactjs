65fde60cbd10149f402f34ca258b5796713d9827
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-01-14 15:10:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Importing components
+import Landing from "./pages/landing";
+
+//Importing stylesheet
+import "sanstrap/dist/css/sanstrap.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Landing} />
+      </div>
+    </Router>
   );
 }
 
